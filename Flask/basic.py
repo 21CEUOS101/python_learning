e5a9c8b7eba94624e9c51df1b410e52d8bc43a59
21CEUOS101 <21ceuos101@ddu.ac.in>
2024-08-10 18:49:13 +0530

    Add file upload and download functionality for user resumes

--- Flask/basic.py
+++ Flask/basic.py
@@ -1,4 +1,4 @@
-from flask import Flask , request
+from flask import Flask , request , send_from_directory , send_file
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -46,6 +46,36 @@ def update_user(id, user):
 def delete_user(id):
     data.pop(id)
     return "User deleted successfully!"
+
+# file upload
+@app.post('/users/resume/<int:id>')
+def upload_resume(id):
+    resume = request.files['resume']
+
+    # check if the file is a pdf
+    if resume.filename.split('.')[-1] != 'pdf':
+        return "Invalid file format. Please upload a pdf file."
+    
+    #check if the file is empty
+    if resume.filename == '':
+        return "No file selected. Please select a file to upload."
+    
+    # check if the file is too large
+    if len(resume.read()) > 1000000:
+        return "File is too large. Please upload a file less than 1MB."
+    
+    # save the file
+    resume.save(f'resume-{id}.pdf')
+    return "Resume uploaded successfully!"
+
+# get the uploaded file
+@app.get('/users/resume/<int:id>')
+def get_resume(id):
+    try:
+        return send_file(f'../resume-{id}.pdf', as_attachment=True)
+    except FileNotFoundError:
+        return "File not found!"
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
